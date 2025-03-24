a4302881be7ee8a9ea09f88048120f4bdee26ef5
Kelvin Hey <kelvinhey@outlook.com>
2025-03-24 07:12:16 -0300

    feat: adiciona usuários em memória

--- src/main/java/com/zeroes/sigupa/core/security/WebSecurityConfig.java
+++ src/main/java/com/zeroes/sigupa/core/security/WebSecurityConfig.java
@@ -1,9 +1,35 @@
 package com.zeroes.sigupa.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder.encode("123"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder.encode("321"))
+                .roles("USER", "ADMIN")
+                .build());
+
+        return manager;
+    }
 }
